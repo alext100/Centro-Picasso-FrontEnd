dbf5113ce094dfeb6ef545e75b570d5240356556
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-12-01 00:00:11 +0100

    Typescript interfaces created

---
+++ src/types/interfaces.ts
@@ -0,0 +1,53 @@
+interface UserModel {
+  id: string;
+  username: string;
+  password: string;
+  email: string;
+  firstname: string;
+  lastname: string;
+  adminAccess: boolean;
+  professorAccess: boolean;
+  studentAccess: boolean;
+  groups: Array<string>;
+  studentErrors: Array<string>;
+}
+interface UserWithToken {
+  user: {
+    id: string;
+    username: string;
+    password: string;
+    email: string;
+    firstname: string;
+    lastname: string;
+    adminAccess: boolean;
+    professorAccess: boolean;
+    studentAccess: boolean;
+    groups: Array<string>;
+    studentErrors: Array<string>;
+  };
+  id: string;
+  token: string;
+  refreshToken: string;
+}
+interface State {
+  user: UserModel;
+  userId: string;
+  token: string;
+  refreshToken: string;
+  isLoggedIn: boolean;
+  currentUser: UserWithToken;
+  isUserAuthenticated: boolean;
+}
+interface UserLoginData {
+  username: string;
+  password: string;
+}
+interface UserRegisterData {
+  username: string;
+  name: string;
+  email: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+}
+export { UserModel, State, UserWithToken, UserLoginData, UserRegisterData };
