68090c885c1173399ceecdc27b4c608c256c9deb
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-12-01 13:33:53 +0100

    To jest.config compilerOptions for custom element added

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  globals: {
+    "vue-jest": {
+      compilerOptions: {
+        isCustomElement: (tag) => tag.startsWith("b-form"),
+      },
+    },
+  },
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   transform: {
     "^.+\\.vue$": "vue-jest",
