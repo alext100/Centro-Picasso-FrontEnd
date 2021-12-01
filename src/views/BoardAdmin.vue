ff9f2e62317480843a044957d74c779428128927
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-12-01 21:43:43 +0100

    Functions of reading teachers list to BoardAdmin added

--- src/views/BoardAdmin.vue
+++ src/views/BoardAdmin.vue
@@ -1,13 +1,40 @@
 <template>
   <div class="container">
-    <h1>Here it will be an admin page</h1>
+    <h1>¡Hola, {{ currentUser.username }}!</h1>
+    <p>
+      Aquí puedes cambiar la información sobre los profesores, sus derechos de acceso. También los precios de los cursos que serán
+      visibles para los visitantes del sitio en la página de Precios. Y también puedes crear un horario de lecciones que solo los
+      profesores verán.
+    </p>
+    <ul>
+      <li v-for="teacher in teachers" :key="teacher">
+        <router-link :to="'/users/' + teacher.id"
+          ><span>{{ teacher.username }}</span></router-link
+        >
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { mapActions, mapState } from "vuex";
 
 export default defineComponent({
   name: "Admin",
+  computed: {
+    ...mapState(["teachers", "currentUser"]),
+  },
+  methods: {
+    ...mapActions(["getTeachersFromApi"]),
+  },
+  mounted() {
+    this.getTeachersFromApi();
+  },
+  data() {
+    return {
+      user: "",
+    };
+  },
 });
 </script>
