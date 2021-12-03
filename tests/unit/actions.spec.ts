a3f7ee6669a3ecb9cfb51cc9bb4fa7d6646ec135
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-12-03 16:04:53 +0100

    Test of updatePriceInDB action added

--- tests/unit/actions.spec.ts
+++ tests/unit/actions.spec.ts
@@ -27,4 +27,66 @@ describe("Given an actions from store", () => {
       expect(commit).toHaveBeenCalledWith("loadPrices", data);
     });
   });
+
+  describe("When the action getTeachersFromApi is invoked", () => {
+    test("Then it should call commit with loadTeachers and received data", async () => {
+      const data = [
+        {
+          username: "maria",
+          password: "$2b$10$gtoDQ8yc/ddtCy45.uMK52RzF.520Bou1zaJpBNoJO",
+          email: "maria@asf",
+          firstname: "María",
+          lastname: "Fernandez",
+          adminAccess: false,
+          professorAccess: true,
+          studentAccess: false,
+          groups: [],
+          studentErrors: [],
+          image: "https://i.ebayimg.com/thumbs/images/g/VbAwFX/s-l300.jpg",
+          id: "61a7c88f0f7c5abg5ha5ea323a",
+        },
+        {
+          username: "manuel",
+          password: "$2b$10$5tXsP5PsR55NM8MO.Ie1kmrJZ4s8PklFr0XUO5nGiRp2YG",
+          email: "manuel@asf",
+          firstname: "Manuel",
+          lastname: "Gomez",
+          adminAccess: false,
+          professorAccess: true,
+          studentAccess: false,
+          groups: [],
+          studentErrors: [],
+          image: "https://fr.shopping.rakuten.com/photo/1444508_L.jpg",
+          id: "61a7c8aa057c5abd5ae5ay23c",
+        },
+      ];
+      mockedAxios.get.mockResolvedValue({
+        data,
+      });
+
+      await actions.getTeachersFromApi(configActionContext(commit));
+
+      expect(commit).toHaveBeenCalledWith("loadTeachers", data);
+    });
+  });
+
+  describe("When the action updatePriceInDB is invoked", () => {
+    test("Then it should call commit with updatedPrice and newPrices", async () => {
+      const data = [
+        {
+          price: 3100,
+          level: "Para tres personas",
+          duration: "1.5 horas",
+          id: "61a9d9f93d1576bdbb9af859",
+        },
+      ];
+      mockedAxios.get.mockResolvedValue({
+        data,
+      });
+
+      await actions.updatePriceInDB(configActionContext(commit), { newPrice: "", priceId: "", priceObject: [] });
+
+      expect(commit).toHaveBeenCalledWith("updatedPrice", data);
+    });
+  });
 });
