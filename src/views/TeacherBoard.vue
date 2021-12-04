0fa00462532d9c186a412ffdcdb6f2cb1317b720
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-12-04 19:18:42 +0100

    Rendering of all groups and user groups to TeacherBoard added

--- src/views/TeacherBoard.vue
+++ src/views/TeacherBoard.vue
@@ -1,18 +1,46 @@
 <template>
-  <h1>¡Hola, {{ currentUser.username }}!</h1>
+  <div class="container">
+    <h1>¡Hola, {{ currentUser.username }}!</h1>
 
-  <p>En esta página puedes formar grupos de tus alumnos. Y para cada grupo, agregue tareas o materiales de estudio.</p>
+    <p>En esta página puedes formar grupos de tus alumnos. Y para cada grupo, agregue tareas o materiales de estudio.</p>
+
+    <div class="container">
+      <h2>Mis grupos:</h2>
+      <ul class="list-group-item-secondary">
+        <li v-for="group in userGroups || []" :key="group" class="container__list">
+          <p>{{ group.groupname }}</p>
+          <b-button pill class="b-card__button mb-5 w-25">Borrar de mis grupos</b-button>
+        </li>
+      </ul>
+    </div>
+
+    <div class="container">
+      <h2>Todos los grupos</h2>
+      <ul class="list-group-item-secondary">
+        <li v-for="group in groups || []" :key="group" class="container__list">
+          <p>{{ group.groupname }}</p>
+          <b-button pill class="b-card__button mb-5 w-25">Añadir a mis grupos</b-button>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { mapState } from "vuex";
+import { mapActions, mapState } from "vuex";
 
 export default defineComponent({
   name: "TeacherBoard",
   computed: {
-    ...mapState(["currentUser"]),
+    ...mapState(["currentUser", "groups", "userGroups"]),
   },
-  methods: {},
+  methods: {
+    ...mapActions(["getGroupsFromApi", "getUserGroupsFromApi"]),
+  },
+  mounted() {
+    this.getGroupsFromApi();
+    this.getUserGroupsFromApi();
+  },
 });
 </script>
