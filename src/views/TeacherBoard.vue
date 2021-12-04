db3d3b3130dae4f34bd32a44f58da195292e1cfa
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-12-04 15:40:31 +0100

    Template of TeacherBoard page created

---
+++ src/views/TeacherBoard.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>¡Hola, {{ currentUser.username }}!</h1>
+
+  <p>En esta página puedes formar grupos de tus alumnos. Y para cada grupo, agregue tareas o materiales de estudio.</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { mapState } from "vuex";
+
+export default defineComponent({
+  name: "TeacherBoard",
+  computed: {
+    ...mapState(["currentUser"]),
+  },
+  methods: {},
+});
+</script>
