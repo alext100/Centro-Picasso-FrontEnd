26a2df9dcfeab6e2adcf68c866e29daf94349eda
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-12-05 02:07:56 +0100

    DkToast installed, styles added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import BootstrapVue3 from "bootstrap-vue-3";
+import DKToast from "vue-dk-toast";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -10,6 +11,17 @@ import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
 const app = createApp(App);
 app.use(BootstrapVue3);
+app.use(DKToast, {
+  duration: 2000,
+  positionY: "bottom",
+  positionX: "right",
+  styles: {
+    color: "#CC1810",
+    backgroundColor: "#FFC107",
+    width: "120px",
+    border: "1px solid #CC1810",
+  },
+});
 app.use(store);
 app.use(router);
 app.mount("#app");
