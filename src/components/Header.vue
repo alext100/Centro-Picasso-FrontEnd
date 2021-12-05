de259bc108f1eb76534ed5703f85e1e89e8ec005
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-12-05 15:00:05 +0100

    In Header to ul of navbar class toast-header added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,5 @@
 <template>
-  <nav
-    id="nav"
-    class="header d-flex flex-column justify-content-between navbar navbar-expand-lg navbar-light bg-light p-3 sticky-md-top"
-  >
+  <nav id="nav" class="header d-flex flex-column justify-content-between navbar navbar-expand-lg navbar-light bg-light p-3 sticky-md-top">
     <div class="header_top offcanvas-body">
       <div class="align-items-md-center offcanvas-body mr-5">
         <img class="header_big-image align-self-md-start ml-2 mr-5" src="../../public/Centro-Picasso-200x91.png" alt="logo" />
@@ -28,7 +25,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="header_navbar-collapse align-self-md-end collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav toast-header">
           <li class="nav-item">
             <router-link class="nav-link m-2" to="/">Inicio</router-link>
           </li>
@@ -54,9 +51,7 @@
             <router-link class="nav-link m-2" to="/login">{{ "Login" }}</router-link>
           </li>
           <li v-if="isUserAuthenticated" class="nav-item">
-            <router-link class="nav-link m-2" to="/"
-              ><button class="logout-button" @click="handleLogout()">Logout</button></router-link
-            >
+            <router-link class="nav-link m-2" to="/"><button class="logout-button" @click="handleLogout()">Logout</button></router-link>
           </li>
           <li v-if="isAdmin" class="nav-item">
             <router-link class="nav-link m-2" to="/admin">{{ "Admin" }}</router-link>
