b0ddfcc569dd3cd04ab7ed2291023d3c7f04fc1b
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-12-05 18:34:04 +0100

    Component GroupInputMessage created

---
+++ src/components/GroupInputMessage.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container-sm d-flex flex-column mt-5">
+    <b-card class="mb-1">
+      <b-form autoComplete="off" action v-on:keydown.enter.prevent="handleAddMessage" data-test="loginBForm">
+        <div>
+          <b-form-group
+            id="message"
+            description="Tu mensaje aquí, por favor"
+            label="mensaje:"
+            label-for="input-message"
+            valid-feedback="¡Gracias!"
+            :invalid-feedback="floatingInvalidFeedbackMessage"
+            :state="floatingStateMessage"
+          >
+            <b-form-textarea
+              size="lg"
+              id="input-message"
+              type="textarea"
+              v-model="message"
+              :state="floatingStateMessage"
+              trim
+              placeholder="Mensaje"
+              rows="6"
+              no-resize
+            ></b-form-textarea>
+          </b-form-group>
+        </div>
+      </b-form>
+    </b-card>
+    <b-button class="login-form--submit-button" @click="handleAddMessage" type="submit" pill>Enviar</b-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, ref } from "vue";
+import { mapActions, mapState } from "vuex";
+import state from "@/store/state";
+
+export default defineComponent({
+  setup() {
+    const message = ref("");
+    const floatingStateMessage = computed(() => message.value.length >= 5);
+    const floatingInvalidFeedbackMessage = computed(() =>
+      message.value.length > 0 ? "Ingrese al menos 5 caracteres" : "Por favor ingrese algo"
+    );
+
+    return {
+      message,
+      floatingStateMessage,
+      floatingInvalidFeedbackMessage,
+    };
+  },
+
+  data() {
+    return {
+      userData: { username: null },
+    };
+  },
+  name: "GroupInputMessage",
+  methods: {
+    ...mapActions(["updateGroup"]),
+    ...mapState(["currentGroup"]),
+    async handleAddMessage() {
+      const groupToUpdate = {
+        groupname: state.currentGroup.groupname,
+        members: state.currentGroup.members,
+        messages: [...state.currentGroup.messages, this.message],
+        id: state.currentGroup.id,
+      };
+      await this.updateGroup(groupToUpdate);
+    },
+  },
+});
+</script>
+
+<style scoped>
+.login-form {
+  max-width: 600px !important;
+}
+button.login-form--submit-button {
+  width: 100px;
+  background-color: #cc1810;
+  color: white;
+}
+</style>
