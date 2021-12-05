2c2e3e889653abfb0a7deeb82a6264624d5d781d
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-12-05 18:39:24 +0100

    Component Group refactored

--- src/views/Group.vue
+++ src/views/Group.vue
@@ -2,46 +2,30 @@
   <div class="container">
     <h1>Group {{ currentGroup.groupname }}</h1>
 
-    <div class="container">
-      <h2>Miembros del grupo:</h2>
-      <ul class="list-group-item-secondary">
-        <li v-for="userFromGroup in loadedUsersFromGroup || []" :key="userFromGroup" class="container__list">
-          <p>{{ userFromGroup.firstname }} {{ userFromGroup.lastname }}</p>
-        </li>
-      </ul>
-    </div>
+    <GroupMembersVue />
 
-    <div class="container">
-      <h2>Mensajes del grupo:</h2>
-      <ul class="list-group-item-secondary">
-        <li v-for="message in currentGroup.messages || []" :key="message" class="container__list">
-          <p>{{ message }}</p>
-        </li>
-      </ul>
-    </div>
+    <GroupInputMessageVue />
+
+    <MessagesVue />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { mapActions, mapState } from "vuex";
-import { UserModel } from "@/types/interfaces";
+import { mapState } from "vuex";
+import MessagesVue from "@/components/GroupMessages.vue";
+import GroupMembersVue from "@/components/GroupMembers.vue";
+import GroupInputMessageVue from "@/components/GroupInputMessage.vue";
 
 export default defineComponent({
   name: "GroupPage",
+  components: {
+    MessagesVue,
+    GroupMembersVue,
+    GroupInputMessageVue,
+  },
   computed: {
     ...mapState(["currentGroup", "loadedUsersFromGroup"]),
-  },
-
-  methods: {
-    ...mapActions(["getGroupById", "getUserById", "deleteLoadedUsers"]),
-  },
-
-  async mounted() {
-    await this.getGroupById(this.$route.params.id);
-    const groupMembers = this.currentGroup.members;
-    await groupMembers.forEach((member: UserModel) => this.getUserById(member));
-    this.deleteLoadedUsers();
   },
 });
 </script>
