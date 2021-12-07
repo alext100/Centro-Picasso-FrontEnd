3c3f143bfa0332f50ffcdf8852bccbebf53f5238
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-12-07 21:49:02 +0100

    To Home page SobreElCentroOnMainPage was imported

--- src/components/CursosOnMainPage.vue
+++ src/components/CursosOnMainPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="align-items-center container d-flex flex-column">
     <h2>Cursos</h2>
-    <b-card-group>
+    <b-card-group class="ml-5">
       <b-card
         title="Cursos grupales"
         img-top
@@ -25,7 +25,7 @@
         ></b-card-img>
       </b-card>
     </b-card-group>
-    <b-card-group>
+    <b-card-group class="mr-5">
       <b-card title="Clubs" img-top tag="article" style="max-width: 20rem" class="mb-2 card-body" border-variant="white" align="center">
         <b-card-img alt="Image" class="card-body--image" src="http://centropicasso.ru/wp-content/uploads/2019/07/%D1%80%D0%B0%D0%B7%D0%B3.png">
         </b-card-img>
@@ -56,14 +56,7 @@ export default defineComponent({
   background-color: transparent;
   color: #cc1810;
 }
-/* .card-body:hover {
-  cursor: default;
-  transform: scale(1.2);
-  transition: all 0.3s ease-in-out 0s;
-} */
 
-.card-body--image {
-}
 .card-body--image:hover {
   transform: scale(1.3);
   transition: all 0.3s ease-in-out 0s;
