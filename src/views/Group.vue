614dc313e8d165ade27cf52bed64dc27c34adc81
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-12-07 09:12:49 +0100

    AddUser import to Group component added

--- src/views/Group.vue
+++ src/views/Group.vue
@@ -2,11 +2,13 @@
   <div class="container">
     <h1>Grupo {{ currentGroup.groupname }}</h1>
 
-    <GroupMembersVue />
-
-    <GroupInputMessageVue />
-
-    <MessagesVue />
+    <b-card>
+      <b-tabs active-nav-item-class="m-2 h-3 list-group-item-success" content-class="mt-3" justified>
+        <b-tab title="Mensajes del grupo" active><GroupInputMessageVue /> <MessagesVue /></b-tab>
+        <b-tab title="Miembros del grupo"><GroupMembersVue /></b-tab>
+        <b-tab title="Añadir un usuario al grupo"><AddUserVue /></b-tab>
+      </b-tabs>
+    </b-card>
   </div>
 </template>
 
@@ -16,6 +18,7 @@ import { mapState } from "vuex";
 import MessagesVue from "@/components/GroupMessages.vue";
 import GroupMembersVue from "@/components/GroupMembers.vue";
 import GroupInputMessageVue from "@/components/GroupInputMessage.vue";
+import AddUserVue from "@/components/AddUser.vue";
 
 export default defineComponent({
   name: "GroupPage",
@@ -23,6 +26,7 @@ export default defineComponent({
     MessagesVue,
     GroupMembersVue,
     GroupInputMessageVue,
+    AddUserVue,
   },
   computed: {
     ...mapState(["currentGroup", "loadedUsersFromGroup"]),
